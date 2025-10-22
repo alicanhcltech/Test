9c2d9c662f3e8da00b60f0abf45e3c6699967004
zeroheight <tokens-management@zeroheight.com>
2025-10-22 14:39:53 -0400

    Update Material 3 Design Kit (Community) from zeroheight

--- ios/token_Collection_Mode1.m
+++ ios/token_Collection_Mode1.m
@@ -6,6 +6,7 @@
 #import "Material3DesignKitCommunity.h"
 
 
+NSString * const ColorBlackblack = [UIColor colorWithRed:0.616f green:0.608f blue:0.608f alpha:1.000f];
 NSString * const ColorBlue = [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f];
 
 @implementation Material3DesignKitCommunity
@@ -17,6 +18,12 @@ NSString * const ColorBlue = [UIColor colorWithRed:1.000f green:1.000f blue:1.00
   dispatch_once(&onceToken, ^{
     array = @[
       @{
+  @"value": [UIColor colorWithRed:0.616f green:0.608f blue:0.608f alpha:1.000f],
+  @"name": @"ColorBlackblack",
+    @"category": @"color",
+    @"type": @"blackblack"
+  },
+@{
   @"value": [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f],
   @"name": @"ColorBlue",
     @"category": @"color",
