c54374b2e99b13fb177007f96e7b7d600fbd7920
zeroheight <tokens-management@zeroheight.com>
2025-10-22 14:29:00 -0400

    Update Material 3 Design Kit (Community) from zeroheight

---
+++ ios/token_Fonttheme_Baseline.swift
@@ -0,0 +1,14 @@
+
+//
+// 
+//
+
+import UIKit
+
+public class Material3DesignKitCommunity {
+    public static let contentStaticFontBrand = "Roboto"
+    public static let contentStaticFontPlain = "Roboto"
+    public static let contentStaticWeightBold = "SemiBold"
+    public static let contentStaticWeightMedium = "Medium"
+    public static let contentStaticWeightRegular = "Regular"
+}
